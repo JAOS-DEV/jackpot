92dc86e5ad380f0d895dfc3488f8db0111eeb77b
J <73671839+JAOS-DEV@users.noreply.github.com>
2021-03-16 20:07:00 +0000

    ios scroll fix
    1

--- indexScript.js
+++ indexScript.js
@@ -1,9 +1,10 @@
 "use strict";
 
-addEventListener(
-  "load",
-  function () {
-    window.scrollTo(1, 0);
-  },
-  false
-);
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+  /* iOS hides Safari address bar */
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      window.scrollTo(0, 1);
+    }, 1000);
+  });
+}
--- script.js
+++ script.js
@@ -31,13 +31,23 @@ let lives = 3;
 let dice1, dice2, bothDice;
 //================================================================================================
 // Start
-addEventListener(
-  "load",
-  function () {
-    window.scrollTo(1, 0);
-  },
-  false
-);
+// addEventListener(
+//   "load",
+//   function () {
+//     window.scrollTo(1, 0);
+//   },
+//   false
+// );
+
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+  /* iOS hides Safari address bar */
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      window.scrollTo(0, 1);
+    }, 1000);
+  });
+}
+console.log(navigator);
 
 // FUNCTIONS
 // Enable Buttons
