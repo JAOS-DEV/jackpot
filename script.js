09d60ecbead41e92ef056df97d4f6b29cb390bc4
J <73671839+JAOS-DEV@users.noreply.github.com>
2021-03-16 21:16:40 +0000

    swipe up twice

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) 
 
   window.addEventListener("load", function () {
     setTimeout(function () {
-      window.scrollTo(0, -1000);
+      window.scrollTo(0, 1000);
     }, 2000);
   });
 }
