3f054998cc9244cc9c888e002a66ca75f0c15cbe
J <73671839+JAOS-DEV@users.noreply.github.com>
2021-04-01 22:21:07 +0100

    Added Special feature
    added feature

---
+++ jackpot - New/indexScript.js
@@ -0,0 +1,10 @@
+"use strict";
+
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+  /* iOS hides Safari address bar */
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      window.scrollTo(0, 1);
+    }, 1000);
+  });
+}
